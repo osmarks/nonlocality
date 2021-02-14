e1f0395e95af276006ac99809c6648624a816794
osmarks <osmarks@protonmail.com>
2021-02-14 22:34:34 +0000

    fix tsquery type

--- src/search.ts
+++ src/search.ts
@@ -9,8 +9,9 @@ const search = async query => {
 
     // TODO: there are possible XSS vulnerabilities from the headline thing here
     // I don't know if there ARE any, as HTML is preparsed beforehand, but it should be checked
+    // Also TODO: add options for each tsquery input type
     const out = await DB`SELECT url, updated, ts_rank_cd(fts, query) AS rank, ts_headline(pageText, query, 'MaxFragments=3,MaxWords=60') AS snippet, pageTitle
-    FROM pages, to_tsquery(${query}) query WHERE fts @@ query
+    FROM pages, websearch_to_tsquery(${query}) query WHERE fts @@ query
     ORDER BY rank DESC LIMIT 100`
 
     const end = util.timestamp()
